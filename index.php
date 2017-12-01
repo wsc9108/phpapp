54791d0ebbb3247e3464fdd9f567dfd545aec6c6
Johnny Wang <johnnwa@microsoft.com>
2017-12-01 09:51:17 +0800

    3

--- index.php
+++ index.php
@@ -1,2 +1,14 @@
-hello
-<?php phpinfo() ?>
+<?php
+$servername = "DESKTOP-MA03PDG";
+$username = "wpuser";
+$password = "!1jing110";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+echo "Connected successfully";
+?>
